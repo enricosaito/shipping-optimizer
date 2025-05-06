f8fc1b0f47e8d3d9f52aaf4ab523f0ab3344584e
Enrico Saito <enricosaito@gmail.com>
2025-05-06 17:07:16 -0300

    feat: update proxy for requests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,13 +7,7 @@ export default defineConfig({
     port: 3000,
     host: true,
     proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-        ws: true,
-      },
+      "/api": "http://localhost:3000",
     },
   },
 });
